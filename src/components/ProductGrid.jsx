794944b030f5eeda3de25a1af1ffcce58d841daf
AnthMan <76910782+Anthvirus@users.noreply.github.com>
2024-12-26 14:48:03 +0100

    Update ProductGrid.jsx

--- src/components/ProductGrid.jsx
+++ src/components/ProductGrid.jsx
@@ -4,7 +4,7 @@ import ProductCard from './ProductCard';
 
 const ProductGrid = () => {
   return (
-    <div className="grid grid-cols-1 gap-4 p-5 lg:mx-4 lg:px-2 sm:grid-cols-2 md:mx-0 md:px-0 md:grid-cols-3 xl:grid-cols-5 min-h-max lg:grid-cols-auto-fit grid-flow-dense">
+    <div className="grid grid-cols-1 gap-4 p-5 lg:mx-4 lg:px-2 sm:grid-cols-2 md:mx-0 md:px-0 md:grid-cols-3 xl:grid-cols-4 min-h-max lg:grid-cols-auto-fit grid-flow-dense">
       {products.map(product => (
         <ProductCard key={product.id} product={product} />
       ))}
